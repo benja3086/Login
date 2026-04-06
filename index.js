8e93c3a2e7e33e575f70eee96f5eee344691450b
benja3086 <benjaminquinteros785@gmail.com>
2026-04-06 19:12:45 -0300

    firt commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { PORT } from './config.js';
+import { UserRepository } from './user-repository.js'; // ← agregá esto
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await UserRepository.login({ username, password });
+        res.send({ user });
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    console.log(req.body);
+
+    try {
+        const id =  await UserRepository.create({ username, password });
+        res.send({ id });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+app.post('/logout', (req, res) => {});
+
+app.get('/protected', (req, res) => {});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
